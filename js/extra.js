6d52276a887392a2dbd6943dad1427bcf6a2aed5
Pablo Grisafi <pablogrisafi1975@gmail.com>
2015-03-20 04:02:57 -0300

    fixed intrapage navigation issue!

--- js/extra.js
+++ js/extra.js
@@ -22,6 +22,13 @@ $(function() {
 	$('#menu-tab a[data-toggle="pill"]').on('shown.bs.tab', function(e) {
 		window.location.hash = 'tab=' + e.target.href.substr(e.target.href.indexOf('#') + 1);
 	});
+	$("a[href*='#tab=']").click(function(e){
+		var selectedTabIndex = this.href.indexOf('=');
+		var selectedTab = this.href.substr(selectedTabIndex + 1);
+		$('#menu-tab a[href="#' + selectedTab + '"]').tab('show');
+		e.preventDefault();
+		return false;
+	});
 	var selectedTabIndex = window.location.hash.indexOf('=');
 	if (selectedTabIndex !== -1) {
 		var selectedTab = window.location.hash.substr(selectedTabIndex + 1);
